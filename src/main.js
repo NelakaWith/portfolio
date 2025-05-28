8452a34e4d0870f1c14d0b98dd059dc2d1e7064c
NelakaWith <nelaka.with@gmail.com>
2025-05-28 16:27:48 +0530

    Updates

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import "./scss/main.scss";
-import App from "./App.vue";
-import { Icon } from "@iconify/vue";
-
-createApp(App).component("Icon", Icon).mount("#app");
